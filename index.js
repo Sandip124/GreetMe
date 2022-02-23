7fb026de78990315f4284b18c7569f9529cc22f0
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2022-02-23 22:04:43 +0545

    fix greeting showing correctly on time

--- index.js
+++ index.js
@@ -15,62 +15,75 @@ const getColorPair = require("random-color-pair");
 
 
 
-app.get('/', async (req, res) => {
+app.get('/',
+    async (req, res) => {
 
-    const {
-        greet,
-        heading,
-        text
-    } = req.query;
+        const {
+            greet,
+            heading,
+            text
+        } = req.query;
 
-    res.setHeader("Cache-Control", `public, max-age=${5}`);
+        res.setHeader("Cache-Control", `public, max-age=${5}`);
 
-    const currentDate = new Date();
-    const hours = currentDate.getHours();
+        const currentDate = new Date();
+        const hours = currentDate.getHours();
 
-    let message = "Welcome to my github page.";
-    let textMessage = "weeeeeeeeeee weeeeeeeeeee";
+        let message = "Welcome to my github page.";
+        let textMessage = "weeeeeeeeeee weeeeeeeeeee";
 
-    if(typeof heading != 'undefined' && heading !== ''){
-        message = heading;
-    }
+        if (typeof heading != 'undefined' && heading !== '') {
+            message = heading;
+        }
 
-    if(typeof text != 'undefined' &&  text !== ''){
-        textMessage = text;
-    }
+        if (typeof text != 'undefined' && text !== '') {
+            textMessage = text;
+        }
 
-    if(typeof greet != 'undefined' && greet === "true") {
-        let randomMessage = hours < 12 ? uniqueRandomArray(morningGreetings.greetings) : hours < 18 ? uniqueRandomArray(dayGreetings.greetings) : hours < 21 ? uniqueRandomArray(eveningGreetings.greetings) : uniqueRandomArray(nightGreetings.greetings);
-        message = randomMessage();
-        textMessage = randomMessage();
-    }
-    const width = 1200
-    const height = 250
+        if (typeof greet != 'undefined' && greet === "true") {
+            //let randomMessage = hours < 12 ? uniqueRandomArray(morningGreetings.greetings) : hours < 18 ? uniqueRandomArray(dayGreetings.greetings) : hours < 21 ? uniqueRandomArray(eveningGreetings.greetings) : uniqueRandomArray(nightGreetings.greetings);
+            let randomMessage;
+            if(hours < 12){
+                randomMessage = uniqueRandomArray(morningGreetings.greetings)
+            }else if(hours >= 12 && hours < 18)
+            {
+                randomMessage = uniqueRandomArray(dayGreetings.greetings)
+            }else if(hours >=18 && hours < 21){
+                randomMessage = uniqueRandomArray(eveningGreetings.greetings)
+            }else{
+                randomMessage = uniqueRandomArray(nightGreetings.greetings)
+            }
+            
+            message = randomMessage();
+            textMessage = randomMessage();
+        }
+        const width = 1200
+        const height = 250
 
-    registerFont("poppins-Regular.ttf",{family:"Poppins",weight:"400",style:"Regular"})
-    registerFont("poppins-Bold.ttf",{family:"Poppins",weight:"700",style:"Bold"})
-    const canvas = createCanvas(width, height)
-    const context = canvas.getContext('2d')
-    const [foreground, background] = getColorPair();
+        registerFont("poppins-Regular.ttf", {family: "Poppins", weight: "400", style: "Regular"})
+        registerFont("poppins-Bold.ttf", {family: "Poppins", weight: "700", style: "Bold"})
+        const canvas = createCanvas(width, height)
+        const context = canvas.getContext('2d')
+        const [foreground, background] = getColorPair();
 
 
-    context.fillStyle = background;
-    roundedRect(context,0,0,width,height,16);
-    context.font = '700 36px Poppins, Sans-Serif;'
-    context.textAlign = 'center'
-    context.textBaseline = 'top'
+        context.fillStyle = background;
+        roundedRect(context, 0, 0, width, height, 16);
+        context.font = '700 36px Poppins, Sans-Serif;'
+        context.textAlign = 'center'
+        context.textBaseline = 'top'
 
-    context.fillStyle = foreground
-    await fillTextWithTwemoji(context, message, 600, 70);
+        context.fillStyle = foreground
+        await fillTextWithTwemoji(context, message, 600, 70);
 
-    context.font = '400 18px Poppins, Sans-Serif;'
-    await fillTextWithTwemoji(context, `${textMessage}`, 600, 150);
+        context.font = '400 18px Poppins, Sans-Serif;'
+        await fillTextWithTwemoji(context, `${textMessage}`, 600, 150);
 
-    const buffer = canvas.toBuffer('image/png')
-    res.contentType('image/jpeg');
-    res.send(buffer);
+        const buffer = canvas.toBuffer('image/png')
+        res.contentType('image/jpeg');
+        res.send(buffer);
 
-})
+    })
 
 function roundedRect(ctx, x, y, width, height, radius) {
     ctx.beginPath();
