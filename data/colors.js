bcd4b87fcf6ac59828a7f02b82c105ba3eb68887
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2022-10-27 09:30:16 +0545

    fix timezone issue + add custom color for ui + and more customizable properites

---
+++ data/colors.js
@@ -0,0 +1,8 @@
+const colorPairs=[
+    ["#06283D","#FFFFFF"],
+    ["#5C2E7E","#FFFFFF"],
+    ["#181818","#FFFFFF"],
+    ["#B1B2FF","#666666"]
+]
+
+module.exports = { colorPairs };
--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-const uniqueRandomArray  = require("unique-random-array");
+const uniqueRandomArray = require("unique-random-array");
 const morningGreetings = require("./data/greetings.js");
 const dayGreetings = require("./data/day.js");
 const eveningGreetings = require("./data/evening.js");
 const nightGreetings = require("./data/night.js");
+const colorPairs = require("./data/colors.js");
 
-const { registerFont,createCanvas } = require('canvas')
+const { registerFont, createCanvas } = require('canvas')
 const { fillTextWithTwemoji } = require('node-canvas-with-twemoji-and-discord-emoji');
-
-const getColorPair = require("random-color-pair");
-
-
 
 app.get('/',
     async (req, res) => {
@@ -21,15 +18,18 @@ app.get('/',
         const {
             greet,
             heading,
-            text
+            text,
+            darkMode = false,
+            w=1200,
+            h=250
         } = req.query;
 
         res.setHeader("Cache-Control", `public, max-age=${5}`);
 
-        const currentDate = new Date();
+        const currentDate = new Date(getDateFromOffset(getTimeZoneOffset()));
         const hours = currentDate.getHours();
 
-        let message = "Welcome to my github page.";
+        let message = "Hello Everyone this is greeter.";
         let textMessage = "weeeeeeeeeee weeeeeeeeeee";
 
         if (typeof heading != 'undefined' && heading !== '') {
@@ -41,32 +41,50 @@ app.get('/',
         }
 
         if (typeof greet != 'undefined' && greet === "true") {
-            //let randomMessage = hours < 12 ? uniqueRandomArray(morningGreetings.greetings) : hours < 18 ? uniqueRandomArray(dayGreetings.greetings) : hours < 21 ? uniqueRandomArray(eveningGreetings.greetings) : uniqueRandomArray(nightGreetings.greetings);
             let randomMessage;
-            if(hours < 12){
+            if (hours < 12) {
                 randomMessage = uniqueRandomArray(morningGreetings.greetings)
-            }else if(hours >= 12 && hours < 18)
-            {
+            } else if (hours >= 12 && hours < 18) {
                 randomMessage = uniqueRandomArray(dayGreetings.greetings)
-            }else if(hours >=18 && hours < 21){
+            } else if (hours >= 18 && hours < 21) {
                 randomMessage = uniqueRandomArray(eveningGreetings.greetings)
-            }else{
+            } else {
                 randomMessage = uniqueRandomArray(nightGreetings.greetings)
             }
-            
+
             message = randomMessage();
             textMessage = randomMessage();
         }
-        const width = 1200
-        const height = 250
 
-        registerFont("poppins-Regular.ttf", {family: "Poppins", weight: "400", style: "Regular"})
-        registerFont("poppins-Bold.ttf", {family: "Poppins", weight: "700", style: "Bold"})
+        let width = 0
+        let height = 0
+
+        if (typeof w != 'undefined' && w !== '') {
+            width = w;
+        }
+
+        if (typeof h != 'undefined' && h !== '') {
+            height = h;
+        }
+
+        registerFont("poppins-Regular.ttf", { family: "Poppins", weight: "400", style: "Regular" })
+        registerFont("poppins-Bold.ttf", { family: "Poppins", weight: "700", style: "Bold" })
         const canvas = createCanvas(width, height)
         const context = canvas.getContext('2d')
-        const [foreground, background] = getColorPair();
 
+        let background;
+        let foreground;
 
+        if (typeof darkMode != 'undefined' && darkMode === "true") {
+            if (darkMode) {
+                background = `#888888`;
+                foreground = `#FFFFFF`;
+            } else {
+                const colors = uniqueRandomArray(colorPairs.colorPairs);
+                background = `${colors()[0]}`;
+                foreground = `${colors()[1]}`;
+            }
+        }
         context.fillStyle = background;
         roundedRect(context, 0, 0, width, height, 16);
         context.font = '700 36px Poppins, Sans-Serif;'
@@ -96,6 +114,34 @@ function roundedRect(ctx, x, y, width, height, radius) {
     ctx.stroke();
 }
 
+const getTimeZoneOffset = () => {
+    const timezoneOffset = new Date().getTimezoneOffset()
+    const offset = Math.abs(timezoneOffset)
+    const offsetOperator = timezoneOffset < 0 ? '+' : '-'
+    const offsetHours = (offset / 60).toString().padStart(2, '0')
+    const offsetMinutes = (offset % 60).toString().padStart(2, '0')
+
+    return `${offsetOperator}${offsetHours}`
+}
+
+function getDateFromOffset(offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // subtract local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    console.log(d.getTimezoneOffset());
+    // create new Date object for different city
+    // using supplied offset
+    var newDate = new Date(utc + (3600000 * offset));
+
+    // return time as a string
+    return newDate.toLocaleString();
+}
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port: ${port}`)
 })
